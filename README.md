f263fe12bfa6930df4b5cec7fb1c6d60efe266a2
程睿 <chengr@phkj168.com>
2018-09-08 22:53:22 +0800

    docker-compose for zabbix

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# docker-compose-for-zabbix
-使用docker-compose部署zabbix监控
+# 运维-服务器监控
+
+本项目的目的是用于docker编排方式快速安装部署zabbix,实现对服务器的监控。
+
+<b>实现思路：</b>
+
+1、通过`docker-compose`创建所需的`zabbix-mysql` 、`zabbix-server`、`zabbix-web`、`zabbix-agent`容器。
+
+2、通过`zabbix-server`向`zabbix-agent`收集监控信息。
+
+3、`docker.sh`记录了普通创建容器的方式进行部署`zabbix`。
+
+
+## 一、环境准备（首次执行）
+
+以下操作只需要第一次在服务器上执行一次，若服务器上已经安装了“Docker Compose”则可以跳过本步骤。
+
+
+### 1.1、运行以下命令下载最新版本的Docker Compose:
+
+如果不能下载成功，则将`https`改为`http`
+
+```
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+```
+
+### 1.2、给Docker Compose应用可执行权限:
+
+```
+sudo chmod +x /usr/local/bin/docker-compose
+```
+## 二、执行部署
+
+1、修改`docker-compose`中`web`服务`ZBX_SERVER_HOST`为`zabbix-server`的IP。
+
+2、在刚才创建的`docker-compose.yml`目录下，执行`docker-compose up -d`，这样就会先后启动`mysql`和`zabbix-server`两个服务的容器。
+
+```
+docker-compose up -d
+```
+查看安装进度
+
+```
+docker logs -f zabbix-server
+```
+
+## 三、页面配置
+
+1、使用`IP:8000`登录`web`页面,帐号`Admin`密码`zabbix`登录。
+
+2、添加主机时，必须使用`agent`的`hostname`,添加监控项或监控模板后，才开始对`agent`进行监控。
+
+3、在被监控服务器上执行以下命令，修改其中的IP为`zabbix-server`的IP，`ZBX_HOSTNAME`指本机命名。
+
+````
+docker run --name zabbix-agent -p 10050:10050 -e ZBX_HOSTNAME="zabbix-luzhou" -e ZBX_SERVER_HOST="47.98.108.18" -e ZBX_SERVER_PORT=10051 -d zabbix/zabbix-agent
+````
---
+++ docker.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+#本次使用docker搭建zabbix的组合是mysql+docker+zabix-server
+#先安装数据库mysql
+
+docker run --name zabbix-mysql-server --hostname zabbix-mysql-server \
+-e MYSQL_ROOT_PASSWORD="123456" \
+-e MYSQL_USER="zabbix" \
+-e MYSQL_PASSWORD="123456" \
+-e MYSQL_DATABASE="zabbix" \
+-p 3306:3306  \
+-d mysql:5.7 \
+--character-set-server=utf8 --collation-server=utf8_bin
+
+#创建zabbix-server
+
+docker run  --name zabbix-server-mysql --hostname zabbix-server-mysql \
+--link zabbix-mysql-server:mysql \
+-e DB_SERVER_HOST="mysql" \
+-e MYSQL_USER="zabbix" \
+-e MYSQL_DATABASE="zabbix" \
+-e MYSQL_PASSWORD="123456" \
+-v /etc/localtime:/etc/localtime:ro \
+-v /data/docker/zabbix/alertscripts:/usr/lib/zabbix/alertscripts \
+-v /data/docker/zabbix/externalscripts:/usr/lib/zabbix/externalscripts \
+-p 10051:10051 \
+-d \
+zabbix/zabbix-server-mysql
+
+#最后安装zabbix-web-nginx
+
+docker run --name zabbix-web-nginx-mysql --hostname zabbix-web-nginx-mysql \
+--link zabbix-mysql-server:mysql \
+--link zabbix-server-mysql:zabbix-server \
+-e DB_SERVER_HOST="mysql" \
+-e MYSQL_USER="zabbix" \
+-e MYSQL_PASSWORD="123456" \
+-e MYSQL_DATABASE="zabbix" \
+-e ZBX_SERVER_HOST="zabbix-server" \
+-e PHP_TZ="Asia/Shanghai" \
+-p 8000:80 \
+-p 8443:443 \
+-d \
+zabbix/zabbix-web-nginx-mysql
+
+#登录访问测试,浏览器访问ip:8000查看
+#默认登录
+#username:Admin
+#password:zabbix
+#监控本机时，需要将“127.0.0.1”改为本机IP；监控agent时，添加（add）主机(ZBX_HOSTNAME)监控模板后，可以开始监控agent了。
+
+#创建Zabbix Java gateway实例
+docker run --name zabbix-java-gateway -t -d zabbix/zabbix-java-gateway:latest
+
+
+#zabbbix-agent的安装以及链接zabbix-server
+#server-local-agent
+docker run --restart=always --name zabbix-agent -p 10050:10050 -e ZBX_HOSTNAME="zabbix-server" -e ZBX_SERVER_HOST="47.98.108.18"  -d zabbix/zabbix-agent
+#server-agent-aws
+docker run --restart=always --name zabbix-agent -p 10050:10050 -e ZBX_HOSTNAME="zabbix-aws" -e ZBX_SERVER_HOST="47.98.108.18" -e ZBX_SERVER_PORT=10051 -d zabbix/zabbix-agent
+#server-agent-xinjie
+docker run --restart=always --name zabbix-agent -p 10050:10050 -e ZBX_HOSTNAME="zabbix-xinjie" -e ZBX_SERVER_HOST="47.98.108.18" -e ZBX_SERVER_PORT=10051 -d zabbix/zabbix-agent
+
+#测试服务器75主机
+#server-agent-server
+docker run --restart=always --name zabbix-agent -p 10050:10050 -e ZBX_HOSTNAME="zabbix-server" -e ZBX_SERVER_HOST="192.168.119.75" -e ZBX_SERVER_PORT=10051 -d zabbix/zabbix-agent
+#server-agent-80
+docker run --restart=always --name zabbix-agent -p 10050:10050 -e ZBX_HOSTNAME="zabbix-80" -e ZBX_SERVER_HOST="192.168.119.75" -e ZBX_SERVER_PORT=10051 -d zabbix/zabbix-agent
